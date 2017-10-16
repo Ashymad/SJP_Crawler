0d7b4f386df651404a9e2c53d6c8e0d0af66deb3
Ashymad <czilukim@o2.pl>
2017-10-16 14:46:17 +0200

    clean up main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,16 @@
 #include <iostream>
-#include <fstream>
 #include "SJPCrawler.h"
 
 using namespace std;
 
 int main(void){
   SJPCrawler SJPC;
-  //ofstream file;
-  //file.open("test.html");
-  cout << SJPC.getCommentsNmb("https://sjp.pl/zds") << endl;
-  //cout << "#" <<SJPC.whenLastComment("https://sjp.pl/zds") << "#" << endl;
-  cout << SJPC.getWordsNmb() << endl;
-  //SJPC.indexWords(1,SJPC.getPageCount(),true);
 
-  //cout << SJPC.getWordsNmb() << endl;
+  // Get number of pages
+  int sjp_page_count = SJPC.getPageCount();
 
-  //string site = SJPC.site2string("https://sjp.pl/%B1%E6%EA%B3%F1%F3%B6%BF%BC");
-  //file.close();
-  //cURLCpp cupp;
-  //cupp.getPage("https://sjp.pl/zds");
-  //cout << cupp.toStr() << endl;
-  //file.close();
-  //system("read -n 1");
+  // Index all pages. This takes very long time.
+  SJPC.indexWords(1,sjp_page_count,true);
+
   return 0;
 }
